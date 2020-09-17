f434d133b404758a231e225077d9147ac554ce83
Dmitry Shpak <jazzmanpw@mail.ru>
2020-09-17 22:12:06 +0300

    [server] put value to storage; CORS; refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,23 @@ import storage from './storage';
 const app = express();
 const port = 8080;
 
-app.get('/notes', async (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.send(await storage.get('notes.json'));
-})
+app
+  .use(function corsHeaders(req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+  })
+  .use(express.json({ type: 'application/json' }))
 
-app.listen(port, () => {
-  console.log(`server started, listening to port ${port}`)
-});
+  .get<'get', string>('/notes', async (req, res) => {
+    res.send(await storage.get('notes.json'));
+  })
+  .put<'put', void, string[]>('/notes', async (req, res) => {
+    await storage.set('notes.json', JSON.stringify(req.body));
+    res.send();
+  })
+
+  .listen(port, () => {
+    console.log(`server started, listening to port ${port}`)
+  });
--- server/src/storage.ts
+++ server/src/storage.ts
@@ -2,9 +2,15 @@ import fs from 'fs';
 import path from 'path';
 
 const storageDir = './storage';
+function getFilePath(name: string): string {
+  return path.resolve(storageDir, name)
+}
 
 export default {
   async get(name: string): Promise<string> {
-    return fs.promises.readFile(path.resolve(storageDir, name), { encoding: 'utf-8' });
+    return fs.promises.readFile(getFilePath(name), { encoding: 'utf-8' });
   },
+  async set(name: string, value: string): Promise<void> {
+    return fs.promises.writeFile(getFilePath(name), value, { encoding: 'utf-8' });
+  }
 }
